921d198b19bdcc263da61850538ecec00155c822
Habibakhatun Pathan <habiba254520pathan@gmail.com>
2026-02-13 09:10:33 +0530

    Cc

--- Day-08/Totaldata.py
+++ Day-08/Totaldata.py
@@ -1,1 +1,7 @@
-
+import pandas as pd
+dt = pd.read_csv("coffee_shop_sales.csv")
+dt["total_price"] = dt["transaction_qty"] * dt["unit_price"]
+t_sales = 0
+t_sales += dt["total_price"].sum()
+print("Total sales: ",t_sales)
+print(dt.head())
